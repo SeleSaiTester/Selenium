a8f75957c1e55096707c34f8244ee7b6d141520d
unknown <bsai2020@gmail.com>
2020-10-17 23:39:15 +0530

    This is latest1

--- src/test/java/com/w2a/testcases/LoginTest.java
+++ src/test/java/com/w2a/testcases/LoginTest.java
@@ -1,14 +1,12 @@
 package com.w2a.testcases;
-
+// this is newly added comment 
 import java.util.Hashtable;
 
 import org.testng.annotations.Test;
 
-import com.w2a.crm.accounts.CreateAccount;
 import com.w2a.pages.HomePage;
 import com.w2a.pages.LoginPage;
 import com.w2a.pages.ZohoAppPage;
-import com.w2a.testcases.BaseTest;
 import com.w2a.utilities.Utilities;
 public class LoginTest extends BaseTest{
 	
